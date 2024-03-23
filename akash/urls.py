8a580498af8cd963d00029fe94b8122ed471239e
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2024-03-23 12:42:57 +0530

    Update urls.py

--- akash/urls.py
+++ akash/urls.py
@@ -1,4 +1,4 @@
-"""shubham URL Configuration
+"""akash URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
